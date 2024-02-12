93871dfc7292e384b25bbd4aaeec6a13091de4f5
Moises Alvarenga <moises.alvarenga@outlook.com>
2024-02-12 20:49:45 -0300

    Feat: Foi implementado o GridView

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/data/dummy_data.dart';
+import 'package:flutter_shop/models/product.dart';
 
 class ProductsOverViewPage extends StatelessWidget {
-  const ProductsOverViewPage({super.key});
+  final List<Product> loaderProducts = dummyProducts;
+  ProductsOverViewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Minha loja',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: GridView.builder(
+          itemCount: loaderProducts.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10),
+          itemBuilder: (ctx, i) => Text(loaderProducts[i].title),
+        ),
+      ),
+    );
   }
 }
